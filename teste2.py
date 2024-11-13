e822fbe0e479ad4a5a880e976bb6ebff8e137b37
Valeria Ferreira <sf.valeria@gmail.com>
2024-11-13 11:47:57 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -9,3 +9,7 @@ st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
 numero = st.slider('Selecione um número', min_value = 0, max_value = 100)
 st.text("Seu número é " + str(numero))
+
+
+satisfacao = st.select_slider("Grau de Satisfação", [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+st.text("Seu grau de satisfação é " + str(satisfacao))
