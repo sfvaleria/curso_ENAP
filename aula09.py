080c9cebbb48cd1190214c08d3fda24eeaa0fff1
Valeria Ferreira <sf.valeria@gmail.com>
2024-11-14 09:15:25 -0300

    Create aula09.py

---
+++ aula09.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame ({
+    'nomeServidor' : ['Adriana', 'Thais', 'Samara'],
+    'salario' : [10000, 25000, 20000]
+})
+
+st.write ('Criando uma tabela')
+st.write(df)
